c87186f171afdc095e239528bd872fcc2a543e2e
Foxy <Marc-PC@DESKTOP-LH96JM3>
2021-02-13 11:52:07 +0100

    PlayerSpawn

---
+++ DungeonPoint/Assets/Scripts/PrefabSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour
+{
+    float nextSpawnTime;
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnPlayer()
+    {
+        Instantiate(player, transform.position, Quaternion.identity);
+    }
+}
